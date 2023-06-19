b630bc93afbe58dd47ef25145cc82c6fa3559b22
RomanKosmyna <roma.kosmyna@gmail.com>
2023-06-19 22:40:35 +0300

    Fixed interface errors in Movie Component

--- src/components/Movies/Movie/MovieRating/MovieRating.tsx
+++ src/components/Movies/Movie/MovieRating/MovieRating.tsx
@@ -2,18 +2,14 @@ import {FC} from "react";
 
 import styles from "./MovieRating.module.css";
 
-interface IRating {
-    vote_average: number;
-}
-
 interface IProps {
-    rating: IRating;
+    rating: number;
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const MovieRating: FC<IProps> = ({rating}) => {
-
+const MovieRating: FC<IProps> = (rating) => {
+    console.log(rating)
     return (
         <div className={styles.ratingContainer}>
             <p className={styles.ratingText}>IMDb Rating:</p>
