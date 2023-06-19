c8e4861ec678021da705166f12cdd8e86e78243a
RomanKosmyna <roma.kosmyna@gmail.com>
2023-06-19 22:53:08 +0300

    Fixed Genre Component

--- src/components/GenresComponent/GenreBadge/GenreBadge.js
+++ src/components/GenresComponent/GenreBadge/GenreBadge.js
@@ -1,12 +1,8 @@
-import {Link} from "react-router-dom";
+const GenreBadge = () => {
 
-import styles from './GenreBadge.module.css';
-
-const GenreBadge = ({genre}) => {
-    const {id,name} = genre;
     return (
-        <div className={styles.genreBadge}>
-            <Link to={`/genres/${id}?${name}`} state={{id: genre.id.toString(), name: name}}>{name}</Link>
+        <div>
+
         </div>
     );
 };
