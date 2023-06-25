a8079b58f8cd6068169648900cb6c2a15af9f279
RomanKosmyna <roma.kosmyna@gmail.com>
2023-06-25 17:18:12 +0300

    Fixed position for mobile navigation, as well as for Genre Menu.

--- src/components/Genres/GenreMenu/GenreMenu.tsx
+++ src/components/Genres/GenreMenu/GenreMenu.tsx
@@ -1,6 +1,7 @@
 import GenreMenuMain from "./GenreMenuMain/GenreMenuMain";
 import GenreMenuTop from "./GenreMenuTop/GenreMenuTop";
 import {useAppSelector} from "../../../hooks";
+import {useEffect} from "react";
 
 import styles from "./GenreMenu.module.css";
 
@@ -11,6 +12,13 @@ interface IProps {
 
 const GenreMenu = ({activeGenre, setter}: IProps) => {
     const {genreMenu} = useAppSelector(state => state.toggleReducer);
+
+    useEffect(() => {
+            genreMenu ?
+                document.body.classList.add("no-overflow") :
+                document.body.classList.remove("no-overflow");
+        }, [genreMenu]
+    );
 
     return (
         <>
