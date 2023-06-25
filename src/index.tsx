1cceae1cf7d5efbee12eb702311c355265abc991
RomanKosmyna <roma.kosmyna@gmail.com>
2023-06-25 17:07:00 +0300

    Added styles for closeIcon in UserInfo. Fixed heights on containers.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
 import {setupStore} from "./redux/store/store";
 import {Provider} from "react-redux";
+import {App} from './App';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 const store = setupStore();
